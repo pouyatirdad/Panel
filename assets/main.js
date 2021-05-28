7835eea64610e10e7abd72561fbbf81cb9d75c98
pouyatirdad <66117587+pouyatirdad@users.noreply.github.com>
2021-05-28 22:40:13 +0430

    add model

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+
+function Delete() {
+    return null;
+}
+
+function Edit() {
+
+    document.getElementById("EditModel").style.display = "block";
+
+}
+
+function EditFalse() {
+
+    document.getElementById("EditModel").style.display = "none";
+
+}
+
+function Chose() {
+    var male = document.getElementById("Male");
+    var female = document.getElementById("Female");
+
+
+
+}
